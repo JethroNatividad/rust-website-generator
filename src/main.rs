4bad85059a15d0a301c54bdde574629d283bc4a2
JethroNatividad <natividad.jet@gmail.com>
2024-02-27 13:37:04 +0800

    write pseudocode

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,35 @@
 // Process: generate html template with author in meta tag, create folder and file for js and css, link to html.
 // output: website skeleton
 fn main() {
-    println!("Hello, world!");
+    // Prompt sitename, "Site name: "
+    // Prompt author, "Author: "
+    // Prompt includeJs, "Include Javascript? (Y/N) (default y): "
+    // Prompt includeCss, "Include CSS? (Y/N) (default y): "
+
+    // Write the top level of HTML to sitename/index.html
+    // <!DOCTYPE html>
+    // <html lang="en">
+    //   <head>
+    //     <meta charset="UTF-8" />
+    //     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    //     <meta name="author" content={author} />
+
+    // if includeCss
+    // create file in sitename/css/index.css
+    // then write to sitename/index.html
+    // <link rel="stylesheet" href="./css/index.css">
+
+    // Continue writing the rest
+    //     <title>Document</title>
+    //   </head>
+    //   <body>
+
+    // if includeJs
+    // create file in sitename/js/main.js
+    // then write to sitename/index.html
+    // <script src="./js/main.js"></script>
+
+    // then write the rest
+    // </body>
+    // </html>
 }
