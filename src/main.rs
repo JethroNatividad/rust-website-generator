9727e9c31bc20a447e9690622fd0470f2e407dcf
JethroNatividad <natividad.jet@gmail.com>
2024-02-27 13:24:59 +0800

    expand problem

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 // A program that generates a website skeleton
+// Inputs: site name, author name, javascript?, css?
+// Process: generate html template with author in meta tag, create folder and file for js and css, link to html.
+// output: website skeleton
 fn main() {
     println!("Hello, world!");
 }
