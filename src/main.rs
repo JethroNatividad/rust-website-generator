026fca2121493894860c570502608f314167406c
JethroNatividad <natividad.jet@gmail.com>
2024-02-28 17:23:11 +0800

    impleemnt get input

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
+use std::io;
+use std::io::Write;
+
 // A program that generates a website skeleton
 // Inputs: site name, author name, javascript?, css?
 // Process: generate html template with author in meta tag, create folder and file for js and css, link to html.
 // output: website skeleton
+
+fn get_input<T: std::str::FromStr>(prompt: &str) -> T {
+    loop {
+        print!("{}", prompt);
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+
+        match input.trim().parse() {
+            Ok(value) => break value,
+            Err(_) => println!("Invalid input. Please try again."),
+        }
+    }
+}
+
 fn main() {
     // Prompt sitename, "Site name: "
     // Prompt author, "Author: "
